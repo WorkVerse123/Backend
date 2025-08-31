e1cbbc01ffc0cc4652db9252d88bb690b64c3766
theTrungg <trungthe200427@gmail.com>
2025-08-31 18:48:20 +0700

    Add exception handler

---
+++ WorkVerse/Application/ExceptionHandler/BaseException.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.ExceptionHandler
+{
+    public class BaseException : Exception
+    {
+        public string ErrorCode { get; }
+        public BaseException(string message, string errorCode = null) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public BaseException(string message, Exception innerException, string errorCode = null)
+        : base(message, innerException)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ WorkVerse/Application/ExceptionHandler/MiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Application.ExceptionHandler
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGlobalExceptionHandler(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+        }
+    }
+}
