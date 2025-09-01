dd697f6cb48cb8e07a2a472090d99b291d915ca1
NgBaNhan <nhan23563@gmail.com>
2025-09-01 14:30:56 +0700

    Add IEmployeeProfileRepository, EmployeeProfileRepository

---
+++ WorkVerse/Application/Interfaces/IRepositories/IEmployeeProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.IRepositories
+{
+    public interface IEmployeeRepository : IGenericRepository<EmployeeProfile>
+    {
+        // Employee profile
+        Task<EmployeeProfile?> GetByIdAsync(int employeeId);                     // GET /employees/{id}
+
+    }
+
+}
---
+++ WorkVerse/Infrastructure/Repositories/EmployeeProfileRepository.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces.IRepositories;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class EmployeeRepository : GenericRepository<EmployeeProfile>, IEmployeeRepository
+    {
+        private readonly WorkVerseDBContext _dbContext;
+        public EmployeeRepository(WorkVerseDBContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+       
+
+        public async Task<EmployeeProfile?> GetByIdAsync(int employeeId)
+        {
+            var result = await _dbContext.EmployeeProfiles.FirstOrDefaultAsync(u => u.EmployeeId == employeeId);
+            return result;
+        }
+
+       
+    }
+}
